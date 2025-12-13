0c3b40399c9379c899ad9f13b04fa39332cb33bd
Shubhankar <shubhankarpandit21@gmail.com>
2025-12-14 00:25:11 +0530

    Update q11.java

--- src/assingment03/q11.java
+++ src/assingment03/q11.java
@@ -1,5 +1,17 @@
 package assingment03;
 
 public class q11 {
+public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		        System.out.print("Enter an integer: ");
+		        int n = in.nextInt();
 
+		        boolean by5 = n % 5 == 0;
+		        boolean by6 = n % 6 == 0;
+
+		        System.out.println("Is " + n + " divisible by 5 and 6? " + (by5 && by6));
+		        System.out.println("Is " + n + " divisible by 5 or 6? " + (by5 || by6));
+		        System.out.println("Is " + n + " divisible by 5 or 6, but not both? " + (by5 ^ by6));
+		    }
 }
+
